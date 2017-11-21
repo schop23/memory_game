fbc927548e306c7c8733705deea3f23e0965a7ef
Shelley Schoppert <schop23@gmail.com>
2017-11-20 20:17:42 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-console.log("Up and running!");
+console.log('Up and running!')
 
-var cardOne = "queen" ;
-var cardTwo = "king" ;
-var cardThree = "queen" ;
-var cardFour = "king" ;
+var cards = ['queen', 'queen', 'king', 'king']
 
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardTwo)
+var cardOne = cards[0]
+var cardTwo = cards[2]
 
+var cardsInPlay = []
+cardsInPlay.push([0])
+    console.log('User flipped ' + cardOne )
+cardsInPlay.push([2]) 
+    console.log('User flipped ' + cardTwo )
+    //if (cardsInPlay.length === 2) {}
+    if (cardsInPlay[0] === cardsInPlay[2]) {
+        alert('You found a match!')
+    }
+        else {
+            alert('Sorry, try again.')
+        }
