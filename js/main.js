9ede2c3b161f661acea224c17c7e0f66704904c4
Shelley Schoppert <schop23@gmail.com>
2017-11-22 12:28:31 -0500

    updated commit

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 console.log('Up and running!')
 
 var cards = ['queen', 'queen', 'king', 'king']
-
-var cardOne = cards[0]
-var cardTwo = cards[2]
-
 var cardsInPlay = []
-cardsInPlay.push([0])
-    console.log('User flipped ' + cardOne )
-cardsInPlay.push([2]) 
-    console.log('User flipped ' + cardTwo )
-    //if (cardsInPlay.length === 2) {}
-    if (cardsInPlay[0] === cardsInPlay[2]) {
-        alert('You found a match!')
-    }
-        else {
-            alert('Sorry, try again.')
-        }
+cardsInPlay.push(cards[0])
+var checkForMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert('You found a match!')
+  } else {
+    alert('Sorry, try again.')
+  }
+}
+var flipCard = function (cardId) {
+  console.log('User flipped ' + cards[cardId])
+  if (cardsInPlay.length === 2) {
+    console.log('You found a match!')
+    checkForMatch()
+  }
+}
+flipCard([0])
+flipCard([2])
