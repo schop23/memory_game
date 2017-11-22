75f6f8c5d17bcec12f213967f547f8d922a0bff7
Shelley Schoppert <schop23@gmail.com>
2017-11-22 15:01:23 -0500

    Final Commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 console.log('Up and running!')
 
-var cards = ['queen', 'queen', 'king', 'king']
+var cards = [
+  {
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  }
+]
 var cardsInPlay = []
-cardsInPlay.push(cards[0])
 var checkForMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert('You found a match!')
@@ -10,12 +30,27 @@ var checkForMatch = function () {
     alert('Sorry, try again.')
   }
 }
-var flipCard = function (cardId) {
-  console.log('User flipped ' + cards[cardId])
+var flipCard = function () {
+  var cardId = this.getAttribute('data-id')
+  console.log('User flipped ' + cards[cardId].rank)
+  console.log('User flipped ' + cards[cardId].cardImage)
+  console.log('User flipped ' + cards[cardId].suit)
+  cardsInPlay.push(cards[cardId].rank)
+  this.setAttribute('src', cards[cardId].cardImage)
+  //making sure 2 cards are being played
   if (cardsInPlay.length === 2) {
     console.log('You found a match!')
     checkForMatch()
   }
 }
-flipCard([0])
-flipCard([2])
+//creating game board
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img')
+    cardElement.setAttribute('src', 'images/back.png')
+    cardElement.setAttribute('data-id', i)
+    cardElement.addEventListener('click', flipCard)
+    document.getElementById('game-board').appendChild(cardElement)
+  }
+}
+createBoard()
